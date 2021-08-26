dc15e973dbb13916e5b1de0ae42912adcdb2a4af
Lee Wilson <leevincentwilson@gmail.com>
2021-08-26 12:31:03 +0100

    updated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-
 test("renders learn react link", () => {
   render(<App />);
-  const titleElement = screen.getByText("Welcome to the Front End Test Suite");
+  const titleElement = screen.getByText(
+    `Welcome to the ${process.env.REACT_APP_COMPANY}: Front End Test Suite`
+  );
   expect(titleElement).toBeInTheDocument();
 });
